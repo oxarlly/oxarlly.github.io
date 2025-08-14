7c7b1e2b347b3879760fccf183fa447a306b777d
oxarlly <jianhua37212022@163.com>
2025-08-14 07:01:12 +0000

    upload

---
+++ js/rybbit.js
@@ -0,0 +1,13 @@
+<link rel="stylesheet" class="aplayer-secondary-style-marker" href="/css/APlayer.min.css"><script src="/js/APlayer.min.js" class="aplayer-secondary-script-marker"></script><script class="meting-secondary-script-marker" src="/js/Meting.min.js"></script>document.addEventListener('pjax:complete', () => {
+  rybbit.pageview(window.location.pathname);
+});
+
+document.querySelectorAll('.recent-post-item').forEach(item => {
+  item.addEventListener('click', () => {
+    rybbit.event('post_click', { title: item.querySelector('.post-title').textContent });
+  });
+});
+
+document.querySelector('#search-button').addEventListener('click', () => {
+  rybbit.event('search', { query: document.querySelector('#search-input').value });
+});
